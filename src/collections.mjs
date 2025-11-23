3c07436742bea8b3cf40cc2251b15cb20c8c39f7
serglevchuk <s.a.levchuk@gmail.com>
2025-11-23 17:58:31 +0300

    Update collections.mjs

--- src/collections.mjs
+++ src/collections.mjs
@@ -1,15 +1,7 @@
-import {stringify} from "node:querystring";
-import cartesian from "cartesian";
-import config from "./config.mjs";
-import fetch from "node-fetch";
-// Упрощенная версия - только спорт на текущую неделю
 export function getQueries() {
-  const today = new Date();
-  const nextWeek = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
-  
   return [{
     city: ['moscow'],
     filter: [],
-    url: `https://afisha.yandex.ru/api/events/rubric/sport?city=moscow&period=interval&from=${today.toISOString().split('T')[0]}&to=${nextWeek.toISOString().split('T')[0]}`
+    url: 'https://afisha.yandex.ru/api/events/rubric/sport?city=moscow&limit=30'
   }];
 }
